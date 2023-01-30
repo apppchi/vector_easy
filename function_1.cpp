071ab6aabcc93bc77c842efd89bd264119de2130
apppchi <112154499+apppchi@users.noreply.github.com>
2023-01-30 19:50:36 +0300

    Add files via upload

---
+++ function_1.cpp
@@ -0,0 +1,55 @@
+#include "easy_list.h"
+
+void itc_even_index_list (const vector <int> &mass, vector <int> &mass2){
+    int i;
+    for(i = 1; i < mass.size(); i+=2){
+        mass2.push_back(mass[i]);
+    }
+}
+
+void itc_even_parts_list(const vector <int> &mass, vector <int> &mass2){
+    int i;
+    for(i = 0; i < mass.size(); i++){
+        if(mass[i] % 2 == 0){
+            mass2.push_back(mass[i]);
+        }
+    }
+}
+
+int itc_positive_list(const vector <int> &mass){
+    int kol;
+    for(int i = 0; i < mass.size(); i++){
+        if(mass[i] > 0){
+            kol++;
+        }
+    }
+    return kol;
+}
+
+int itc_sl_list(const vector <int> &mass){
+    int kol = 1;
+    for(int i = 1; i < mass.size(); i++){
+        if(mass[i] > mass[i-1]){
+            kol++;
+        }
+    }
+    if (kol == 1){
+        kol = 0;
+    }
+    return kol;
+}
+
+bool itc_same_parts_list(const vector <int> &mass){
+    int kol;
+    for(int i = 0; i < mass.size(); i++){
+        if((mass[i] > 0 && mass[i+1] > 0) || (mass[i] < 0 && mass[i+1] < 0)){
+            kol++;
+        }
+    }
+    if (kol > 0){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
---
+++ function_2.cpp
@@ -0,0 +1,52 @@
+#include "easy_list.h"
+
+void itc_rev_list(vector <int> &mass){
+    int len = mass.size();
+    for(int i = 0; i < len / 2; i++){
+        mass[i] = mass[i] + mass[len - 1 - i];
+        mass[len - 1 - i] = mass[i] - mass[len - 1 - i];
+        mass[i] = mass[i] - mass[len - 1 - i];
+    }
+}
+
+ void itc_rev_par_list(vector <int> &mass){
+    int len = mass.size();
+    for(int i = 0; i < len - len % 2; i+=2){
+        mass[i] = mass[i] + mass[i + 1];
+        mass[i + 1] = mass[i] - mass[i + 1];
+        mass[i] = mass[i] - mass[i + 1];
+    }
+ }
+
+void itc_rshift_list(vector <int> &mass){
+    int len = mass.size();
+    int c = mass[len - 1];
+    for(int i = len - 1; i > 0; i--){
+        mass[i] = mass[i - 1];
+    }
+    mass[0] = c;
+}
+
+ void itc_super_shift_list(vector <int> &mass, int n){
+    int len = mass.size();
+    int k, p;
+    if (n < 0){
+        n = -n;
+        for(p = 0; p < n; p++){
+            k = mass[0];
+            for(int i = 0; i < len - 1; i++){
+                mass[i] = mass[i + 1];
+            }
+            mass[len - 1] = k;
+        }
+    }
+    else{
+        for(p = 0; p < n; p++){
+            k = mass[len - 1];
+            for(int i = len - 1; i > 0; i--){
+                mass[i] = mass[i - 1];
+            }
+            mass[0] = k;
+        }
+    }
+}
